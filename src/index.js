b3b0cf71b4a5c83bfd4c125ce072a3b9d5c86338
freefaller69 <freefaller@gmail.com>
2021-05-20 21:17:52 -0700

    add babel to project

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ const elvenShieldRecipe = {
   refinedMoonstone: 4,
 }
 
+const elvenGauntletsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 3,
+}
+
 console.log("elvenShieldRecipe", elvenShieldRecipe);
+console.log("elvenGauntletsRecipe", elvenGauntletsRecipe);
